ce07dc4e4d411fe559c3587b88df03fb25f7715d
Diogo França <diogofranca845@gmail.com>
2021-06-09 20:51:15 -0300

    added new files

---
+++ aula11.1/js/script.js
@@ -0,0 +1,6 @@
+const num1 = prompt("Digite um número ?")
+const num2 = prompt("Digite um segundo número ?")
+
+const resultado = Number(num1) + Number(num2)
+
+alert(`O resultado entre ${num1} + ${num2} = ${resultado}`)
---
+++ aula12/aula12.js
@@ -0,0 +1,23 @@
+let varA = 'A'; // B
+let varB = 'B'; // C
+let varC = 'C'; // A
+
+// varA = varA + varB + varC
+
+// varC = varA[0]
+// varB = varA[2]
+// varA = varA[1]
+
+// console.log(varA, varB, varC)
+
+
+const a = varB
+const b = varC
+const c = varA
+
+varA = a
+varB = b
+varC = c
+
+console.log(varA, varB, varC)
+
