94c796850f4765d3e2b0930f0a869e31a782db1a
Diogo França <diogofranca845@gmail.com>
2021-06-19 20:08:24 -0300

    curto circuito

---
+++ aula23/index.js
@@ -0,0 +1,31 @@
+/**
+&& -> false && true -> false "o valor mesmo"
+|| -> true || false -> vai retornar "o valor verdadeiro"
+
+FALSY values - esses valores pode ser valiados como falsos.
+False -> literal
+0
+'' "" ``
+null / undefined
+NaN
+ */
+
+// console.log('luiz' && NaN && 'Maria');
+
+// function falaOi () {
+//     return 'Oi';
+// }
+
+// const vaiExecutar = 'Joãozinho';
+
+// console.log(vaiExecutar && falaOi());
+
+// console.log(0 || false || null || 'Diogo França' || true);
+
+// OR
+
+// const corUsuario = null;
+// const corPadrao = corUsuario || 'preto';
+
+// console.log(corPadrao)
+
