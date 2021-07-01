82b22caf7c0a58a317731827605e8a479062b7aa
Diogo França <diogofranca845@gmail.com>
2021-06-30 21:50:27 -0300

    update files

---
+++ aula25/index.js
@@ -0,0 +1,24 @@
+const numero = 10;
+
+// Se (numero >= 0 && numero <= 5) ocorrer, faça isso {código}
+// Se não faça isso {o código}
+
+// if(numero >= 0 && numero <= 5){
+//     console.log('O número está entre 0 e 5');
+// } else {
+//     console.log('O número NÃO está entre 0 e 5');
+// }
+
+// A primeira condição que for verdadeira será executada, independente se houver outra condição após dentro do bloco if.
+
+if (numero >= 0 && numero <= 5) {
+    console.log('O número está entre 0 e 5');
+} else if (numero >= 6 && numero <= 8) {
+    console.log('O número está entre 6 e 8.');
+} else if (1 === 1) {
+    console.log('LITERAL')
+} else if (numero >= 9 && numero <= 11) {
+    console.log('O número está entre 9 e 11.');
+} else {
+    console.log('O número não está entre 0 e 11.');
+}
